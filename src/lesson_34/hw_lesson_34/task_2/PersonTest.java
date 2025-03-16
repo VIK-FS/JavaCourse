2231acb5dd581fb74761ef6b44245a319a2c73c9
VitaliiK <whitetech0047@gmail.com>
2025-03-16 21:12:03 +0100

    Home_work_lesson_34 (2) "Валидные и невалидные пароли тестируются в отдельных методах"

--- src/lesson_34/hw_lesson_34/task_2/PersonTest.java
+++ src/lesson_34/hw_lesson_34/task_2/PersonTest.java
@@ -1,47 +1,43 @@
 package lesson_34.hw_lesson_34.task_2;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 
 public class PersonTest {
 
-    @Test
-    public void testPasswordValidation() {
-         
-        String[] validPasswords = {
+    static Stream<String> validPasswords() {
+        return Stream.of(
                 "Password123!",
                 "Secure@2024",
                 "Abc123!@#",
                 "Test$Pass1"
-        };
+        );
+    }
 
-        for (String password : validPasswords) {
-            Person person = new Person("user@example.com", password);
-            assertEquals(password, person.getPassword(), "Пароль должен быть установлен: " + password);
-        }
-
-         
-        String[] invalidPasswords = {
+    static Stream<String> invalidPasswords() {
+        return Stream.of(
                 "short1!",
                 "nouppercase123!",
                 "NOLOWERCASE123!",
                 "NoDigits!!",
                 "NoSpecialChar123"
-        };
-
-        for (String password : invalidPasswords) {
-            Person person = new Person("user@example.com", password);
-            assertNull(person.getPassword(), "Пароль не должен быть установлен: " + password);
-        }
+        );
     }
 
-    @Test
-    public void testNullAndEmptyPasswords() {
-        Person person1 = new Person("user@example.com", null);
-        assertNull(person1.getPassword(), "Пароль не должен быть установлен (null)");
+    @ParameterizedTest
+    @MethodSource("validPasswords")
+    public void testValidPasswords(String password) {
+        Person person = new Person("user@example.com", password);
+        assertEquals(password, person.getPassword(), "Пароль должен быть установлен: " + password);
+    }
 
-        Person person2 = new Person("user@example.com", "");
-        assertNull(person2.getPassword(), "Пароль не должен быть установлен (пустая строка)");
+    @ParameterizedTest
+    @MethodSource("invalidPasswords")
+    public void testInvalidPasswords(String password) {
+        Person person = new Person("user@example.com", password);
+        assertNull(person.getPassword(), "Пароль не должен быть установлен: " + password);
     }
 }
-
